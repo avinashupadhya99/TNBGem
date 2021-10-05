7da7da883cc2f48cfbe4d7eb599f7e7ff6c9bd67
avinashupadhya99 <avinashupadhya99@gmail.com>
2021-10-05 06:44:48 +0000

    test: account creation with existing signing key

--- test/account_test.rb
+++ test/account_test.rb
@@ -14,4 +14,18 @@ class TestAccount < Minitest::Test
     account = Thenewboston::Account.new
     assert_equal 64, account.signing_key_hex.length, "Signing key hex should be of 64 length"
   end
+
+  def test_that_account_can_be_created_with_an_existing_signing_key
+    signing_key = "15fbd07fc5e5764bf5919ad41151066f0708e9e2dc11dd42dd1dd671e9485b20"
+    account = Thenewboston::Account.new(signing_key)
+    assert_equal signing_key, account.signing_key_hex, "Signing key hex of the account created should be the same as
+    the signing key used for creation"
+  end
+
+  def test_that_account_cannot_be_created_if_existing_signing_key_is_not_64_characters
+    err = assert_raises RuntimeError do
+      Thenewboston::Account.new("bf5919ad41151066f")
+    end
+    assert_match "Signing key provided should be 64 characters", err.message
+  end
 end
