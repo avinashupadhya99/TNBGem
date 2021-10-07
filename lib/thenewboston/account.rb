cf8aa1adcd2ce01ff2b523d5084e520d2c1118b4
avinashupadhya99 <avinashupadhya99@gmail.com>
2021-10-07 08:55:01 +0000

    refactor: conversion functions to new module

--- lib/thenewboston/account.rb
+++ lib/thenewboston/account.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 require "rbnacl"
 
@@ -12,20 +12,21 @@ module Thenewboston
       elsif signing_key_seed.to_s.length != 64
         raise "Signing key provided should be 64 characters"
       else
-        @signing_key = RbNaCl::SigningKey.new(str_to_key(signing_key_seed))
+        @signing_key = RbNaCl::SigningKey.new(Thenewboston::Util::Conversions.str_to_key(signing_key_seed))
       end
       @account_number = @signing_key.verify_key
 
-      @account_number_hex = key_to_str(@account_number.to_bytes)
-      @signing_key_hex = key_to_str(@signing_key.to_bytes)
+      @account_number_hex = Thenewboston::Util::Conversions.key_to_str(@account_number.to_bytes)
+      @signing_key_hex = Thenewboston::Util::Conversions.key_to_str(@signing_key.to_bytes)
     end
 
     def create_signature(message)
-      key_to_str(@signing_key.sign(message))
+      Thenewboston::Util::Conversions.key_to_str(@signing_key.sign(message))
     end
 
     def create_signed_message(message)
-      signature = key_to_str(@signing_key.sign(hash_to_str(message)))
+      message_str = Thenewboston::Util::Conversions.hash_to_str(message)
+      signature = Thenewboston::Util::Conversions.key_to_str(@signing_key.sign(message_str))
       {
         data: message,
         node_identifier: @account_number_hex,
@@ -37,22 +38,6 @@ module Thenewboston
       new(signing_key_string).account_number_hex == account_number_string
     rescue StandardError
       false
-    end
-
-    def key_to_str(key)
-      key.each_byte.map { |byte| format("%<byte>02x", byte: byte) }.join
-    end
-
-    def str_to_key(str)
-      "".tap { |binary| str.scan(/../) { |hn| binary << hn.to_i(16).chr } }
-    end
-
-    def hash_to_str(hash)
-      if hash.is_a?(String)
-        "\"#{hash}\""
-      else
-        "{" + hash.map { |k, v| "\"#{k}\":#{hash_to_str(v)}" }.join(",") + "}"
-      end
     end
   end
 end
---
+++ lib/thenewboston/utils/conversions.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+module Thenewboston
+  # Utility functions
+  module Util
+    # Functions for string conversions
+    module Conversions
+      def self.key_to_str(key)
+        key.each_byte.map { |byte| format("%<byte>02x", byte: byte) }.join
+      end
+
+      def self.str_to_key(str)
+        "".tap { |binary| str.scan(/../) { |hn| binary << hn.to_i(16).chr } }
+      end
+
+      def self.hash_to_str(hash)
+        if hash.is_a?(String)
+          "\"#{hash}\""
+        else
+          "{" + hash.map { |k, v| "\"#{k}\":#{hash_to_str(v)}" }.join(",") + "}"
+        end
+      end
+    end
+  end
+end
