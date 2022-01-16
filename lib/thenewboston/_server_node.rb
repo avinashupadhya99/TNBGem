9896dd9ce3b724ef664b6ff758df57a9ed5d95b7
avinashupadhya99 <avinashupadhya99@gmail.com>
2022-01-16 05:07:54 +0000

    feat: get bank transactions

--- lib/thenewboston/_server_node.rb
+++ lib/thenewboston/_server_node.rb
@@ -2,6 +2,7 @@
 
 require "uri"
 require "net/http"
+require "json"
 
 module Thenewboston
   # Internal class for all server nodes
@@ -11,6 +12,21 @@ module Thenewboston
     def initialize(url, options = {})
       @url = Thenewboston::Util::URL.format_url(url)
       @options = Thenewboston::Util::Options.format_default_options(options)
+    end
+
+    def get_data(endpoint, params = {})
+      uri = URI("#{@url}#{endpoint}")
+      uri.query = URI.encode_www_form(params)
+
+      res = Net::HTTP.get_response(uri)
+      JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    end
+
+    def get_paginated_data(endpoint, options)
+      self.get_data(endpoint, {
+          **@options[:defaultPagination],
+          **options
+      })    
     end
   end
 end
--- lib/thenewboston/bank.rb
+++ lib/thenewboston/bank.rb
@@ -3,5 +3,8 @@
 module Thenewboston
   # Interact with banks for thenewboston
   class Bank < ServerNode
+    def get_transactions(options = {})
+        get_paginated_data("/bank_transactions", options)
+    end
   end
 end
